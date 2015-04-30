815d5fa9081ef0f5cc0e4139af2144a815f4066e
Justin Chan <justinkhchan@gmail.com>
2015-04-30 05:32:09 +0000

    putting list as homepage

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -24,7 +24,7 @@ class WelcomeController extends Controller {
 	 */
 	public function __construct()
 	{
-		$this->middleware('guest');
+		//$this->middleware('guest');
 	}
 
 	/**
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+Route::get('/', 'WelcomeController@displayTable');
 
 Route::get('home', 'HomeController@index');
 
